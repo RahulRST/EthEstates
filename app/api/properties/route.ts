0ba376d3f9e0ec5fe2175f09bd5018274daf7923
RahulRST <srahuljayaraj@gmail.com>
2024-05-13 19:02:23 +0530

    Dummy property list

--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -2,16 +2,38 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 interface Property {
   id: number;
-  image: string;
+  // image: string;
   name: string;
   location: string;
   price: string;
 }
 
+const properties: Property[] = [
+  {
+    id: 1,
+    // image: "https://noun-api.com/beta/pfp?name=Property1",
+    name: "Beachfront Paradise",
+    location: "Miami, Florida",
+    price: "5,000 ETH/month",
+  },
+  {
+    id: 2,
+    // image: "https://noun-api.com/beta/pfp?name=Property2",
+    name: "Cozy City Apartment",
+    location: "New York City, NY",
+    price: "2.5 ETH/month",
+  },
+  {
+    id: 3,
+    // image: "https://noun-api.com/beta/pfp?name=Property1",
+    name: "Mountain Retreat Cabin",
+    location: "Lake Tahoe, California",
+    price: "1.0 ETH/week",
+  },
+];
+
+
 export async function GET(req: NextApiRequest, res: NextApiResponse<Property[]>) {
-  const properties: Property[] = [
-    { id: 1, image: "https://noun-api.com/beta/pfp?name=Property1", name: "Property 1", location: "...", price: "..." },
-  ];
   return Response.json({
     status: 200,
     properties
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { DynamicProvider, DynamicWidget } from "@/components";
-import Image from "next/image";
+import { DynamicProvider, Footer, Header } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,27 +9,6 @@ export const metadata: Metadata = {
   title: "EthEstates",
   description: "A Web3 Real Estate Marketplace",
 };
-
-const Header = () => {
-  return (
-    <div className="flex m-5 items-center gap-x-6 rounded-lg p-6 justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
-      <p>
-        <span className="font-bold">EthEstates</span>
-      </p>
-      <DynamicWidget />
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <footer className="flex w-full justify-center p-8 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:bg-none">
-      <p className="text-sm text-gray-500 dark:text-gray-400">
-        © {new Date().getFullYear()} EthEstates. All rights reserved.
-      </p>
-    </footer>
-  )
-}
 
 export default function RootLayout({
 children,
