50d6cdf0b64d003dd03b774bc3f742bd6862cccb
RahulRST <srahuljayaraj@gmail.com>
2024-05-19 20:36:35 +0530

    complete alchemy removal

--- components/AccountManager.tsx
+++ components/AccountManager.tsx
@@ -1,55 +1,32 @@
 "use client"
 
-import { getDefaultLightAccountFactoryAddress } from "@alchemy/aa-accounts";
-import { createLightAccountAlchemyClient } from "@alchemy/aa-alchemy";
-import { WalletClientSigner, arbitrumSepolia, type SmartAccountSigner } from "@alchemy/aa-core";
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import { Button, Chip, CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
-// a smart account signer you can use as an owner on ISmartContractAccount
-
 export const AccountManager = () => {
   const { primaryWallet } = useDynamicContext();
-  const [ useLightAccount, setUseLightAccount ] = useState(false);
-  const [ lightAccount, setLightAccount ] = useState<any>();
+  const [ useSmartAccount, setUseSmartAccount ] = useState(false);
+  const [ smartAccount, setSmartAccount ] = useState<any>();
   const [ connectedAddress, setConnectedAddress ] = useState<`0x${string}` | undefined>(undefined);
   const { address } = useAccount();
 
   useEffect(() => {
     setConnectedAddress(undefined)
-    if (useLightAccount) {
-      const getLightAccount = async () => {
-        const dynamicProvider: any = await primaryWallet?.connector?.getWalletClient();
-        const dynamicSigner: SmartAccountSigner = new WalletClientSigner(
-          dynamicProvider,
-          "dynamic" // signer type
-        );
-        const lightAccountClient = await createLightAccountAlchemyClient({
-            apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
-            chain: arbitrumSepolia,
-            signer: dynamicSigner,
-            useSimulation: true,
-            factoryAddress: getDefaultLightAccountFactoryAddress(arbitrumSepolia),
-            version: "v2.0.0"
-          });
-        setLightAccount(lightAccountClient);
-        console.log(lightAccountClient)
-        setConnectedAddress(lightAccountClient.getAddress());
-      };
-        getLightAccount();
+    if (useSmartAccount) {
+      setConnectedAddress("0xSmartAccount")
     } else {
       setConnectedAddress(address!);
     }
-  }, [useLightAccount, address, primaryWallet?.connector]);
+  }, [useSmartAccount, address, primaryWallet?.connector]);
 
   return (
     <div>
       <div className="flex flex-row gap-x-5">
         Connected Address: <Chip variant="outlined" color="primary" label={connectedAddress == undefined ? <CircularProgress size={12} /> : connectedAddress}></Chip>
-        <Button variant="outlined" onClick={() => setUseLightAccount(!useLightAccount)}>
-          {useLightAccount ? "Use Wallet Account" : "Use Light Account"}
+        <Button variant="outlined" onClick={() => setUseSmartAccount(!useSmartAccount)}>
+          {useSmartAccount ? "Use Wallet Account" : "Use Smart Account"}
         </Button>
       </div>
     </div>
--- lib/Provider.tsx
+++ lib/Provider.tsx
@@ -4,46 +4,32 @@ import { createConfig as wagmiCreateConfig, WagmiProvider } from "wagmi";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
-import { AlchemyClientState, createConfig } from "@alchemy/aa-alchemy/config";
-import { AlchemyAccountProvider } from "@alchemy/aa-alchemy/react";
 
 import { http } from "viem";
 import {
   arbitrum,
-  arbitrumSepolia as wArbitrumSepolia,
+  arbitrumSepolia,
   goerli,
   mainnet,
   sepolia,
 } from "viem/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
-import { arbitrumSepolia } from "@alchemy/aa-core";
 
 export const wagmiConfig = wagmiCreateConfig({
-  chains: [mainnet, goerli, wArbitrumSepolia, sepolia, arbitrum],
+  chains: [mainnet, goerli, arbitrumSepolia, sepolia, arbitrum],
   multiInjectedProviderDiscovery: false,
   transports: {
     [mainnet.id]: http(),
     [goerli.id]: http(),
-    [wArbitrumSepolia.id]: http(),
+    [arbitrumSepolia.id]: http(),
     [sepolia.id]: http(),
     [arbitrum.id]: http(),
   },
 });
 
-export const config = createConfig({
-  rpcUrl: "/api/rpc", // this will proxy requests through the app's backend via NextJS routing to hide the Alchemy API key
-  chain: arbitrumSepolia,
-  // because we're using NextJS which is SSR, we need the following settings
-  ssr: true,
-  // This will allow us to persist user state to cookies, making a smoother user experience
-  // not required in non SSR settings
-  // storage: cookieStorage,
-});
-
 const queryClient = new QueryClient();
 
-export const Provider = (props: PropsWithChildren<{ initialState?: AlchemyClientState }>) => {
+export const Provider = ({ children } : { children: any }) => {
   return (
     <DynamicContextProvider
       settings={{
@@ -53,13 +39,7 @@ export const Provider = (props: PropsWithChildren<{ initialState?: AlchemyClient
     >
       <WagmiProvider config={wagmiConfig}>
         <QueryClientProvider client={queryClient}>
-        <AlchemyAccountProvider
-          config={config}
-          queryClient={queryClient}
-          initialState={props.initialState}
-        >
-          <DynamicWagmiConnector>{props.children}</DynamicWagmiConnector>
-        </AlchemyAccountProvider>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
         </QueryClientProvider>
       </WagmiProvider>
     </DynamicContextProvider>
